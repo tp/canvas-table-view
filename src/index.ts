613282953446c4e50786f9a9be28da6b6e017859
Timm Preetz <timm@preetz.us>
2017-03-07 20:10:14 +0100

    Overlay text area for editing the current table cell

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,17 @@ const CANVAS_BACKGROUND_COLOR = '#dedede';
 
 document.body.style.overflow = 'hidden';
 
+const textarea = document.createElement('textarea');
+textarea.style.position = 'absolute';
+textarea.style.top = '0';
+textarea.style.left = '0';
+textarea.style.resize = 'none';
+textarea.style.padding = '0';
+textarea.style.boxSizing = 'border-box';
+textarea.style.fontFamily = 'sans-serif';
+textarea.style.fontSize = '11px';
+document.body.appendChild(textarea);
+
 const canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
 canvas.width = canvasWidth * dpr;
@@ -30,6 +41,8 @@ ctx.moveTo(50, 50);
 ctx.lineTo(Math.random() * 50, Math.random() * 50);
 ctx.stroke();
 
+type EditingColumn = { row: number, column: number };
+let editingColumn: EditingColumn | null = null;
 
 /// Drawing the column headers, evenly spaced
 
@@ -71,11 +84,14 @@ ctx.stroke();
 //   return false;
 // });
 
-const rows = _.range(1, 1001).map((i) => `Row #${i}`);
+const rows = _.range(1, 1001).map((i) => [`(0, ${i})`, `(1, ${i})`, `(2, ${i})`, `(3, ${i})`, `(4, ${i})`]);
 const rowHeight = 15;
 let scrollOffset = 0;
 const MAX_SCROLL_OFFSET = rows.length * rowHeight - canvasHeight + columnHeaderHeight;
 console.debug(`MAX_SCROLL_OFFSET = ${MAX_SCROLL_OFFSET}`);
+
+textarea.style.width = `${columnWidth}px`;
+textarea.style.height = `${rowHeight}px`;
 
 const draw = () => {
   console.debug(`Drawing for scrollOffset = ${scrollOffset}`);
@@ -86,28 +102,58 @@ const draw = () => {
 
   const firstFullRowIndex = Math.floor(scrollOffset / rowHeight);
   const firstPartialRowHeight = scrollOffset % rowHeight;
-  const maxNumberOfRowsToDraw = Math.round((canvasHeight - columnHeaderHeight) / rowHeight);
+  const maxNumberOfRowsToDraw = Math.round((canvasHeight - columnHeaderHeight) / rowHeight) + 0.5;
+  const tableWidth = columnWidth * columnHeaders.length;
 
   console.debug(`firstFullRowIndex = ${firstFullRowIndex} maxNumberOfRowsToDraw = ${maxNumberOfRowsToDraw}`);
+
+  if (
+    editingColumn &&
+    editingColumn.row >= firstFullRowIndex &&
+    editingColumn.row <= firstFullRowIndex + maxNumberOfRowsToDraw // &&
+    // textarea.style.display === 'none'
+  ) {
+    // textarea.style.display = 'block';
+  } else {
+    textarea.style.transform = `translate3d(0px, -50px, 0)`;
+  }
 
   for (let i = 0; i < maxNumberOfRowsToDraw; i++) {
     if (i === 0) {
       ctx.save();
       // clip the first row
-      ctx.rect(0, columnHeaderHeight, columnWidth, rowHeight)
+      ctx.rect(0, columnHeaderHeight, tableWidth, rowHeight)
       ctx.clip();
     }
 
     const elementIndex = (i + firstFullRowIndex);
     const cellBackgroundColor = elementIndex % 2 === 0 ? '#c0c0c0' : '#fefefe';
+    console.assert(rows[elementIndex] != undefined, 'row exists');
+    const rowData = rows[elementIndex];
+    console.assert(rowData.length === columnHeaders.length);
     
     ctx.fillStyle = cellBackgroundColor;
     const y = columnHeaderHeight - firstPartialRowHeight + i * rowHeight;
-    ctx.fillRect(0, y, columnWidth, rowHeight);
+    // draw row background
+    ctx.fillRect(0, y, tableWidth, rowHeight);
+
     ctx.fillStyle = '#000000';
     ctx.font = '11px sans-serif';
-    console.assert(rows[elementIndex] != undefined, 'row exists');
-    ctx.fillText(rows[elementIndex], padding, y + 3);
+
+    for (const [colIndex, column] of rowData.entries()) {
+      ctx.fillText(rowData[colIndex], columnWidth * colIndex + padding, y + 3 /* to align the text vertically centered */);
+
+      if (editingColumn && editingColumn.row === i + firstFullRowIndex && editingColumn.column === colIndex) {
+        // textarea.style.left = `${canvas.offsetLeft + columnWidth * colIndex + padding}px`;
+        // textarea.style.top = `${y + canvas.offsetTop}px`;
+
+        const left = canvas.offsetLeft + columnWidth * colIndex + padding;
+        const top = y + canvas.offsetTop;
+        textarea.style.transform = `translate3d(${left}px, ${top}px, 0)`;
+        textarea.value = rowData[colIndex];
+        textarea.focus();
+      }
+    }
 
     if (i === 0) {
       // reset clip
@@ -123,5 +169,108 @@ canvas.addEventListener('wheel', (ev) => {
 
   requestAnimationFrame(() => draw());
 });
+
+canvas.addEventListener('click', (ev) => {
+  ev.preventDefault()
+  if (ev.button !== 0) {
+    console.debug('ignoring non primary button click');
+    return;
+  }
+  const t = (ev.currentTarget as HTMLCanvasElement);
+  const p = { x: ev.clientX - t.offsetLeft, y: ev.clientY - t.offsetTop};
+
+  const valueClickY = scrollOffset - columnHeaderHeight + p.y;
+  const firstFullRowIndex = Math.floor(valueClickY / rowHeight);
+
+  const column = Math.floor(p.x / columnWidth);
+  console.log(rows[firstFullRowIndex][column]);
+
+  editingColumn = { row: firstFullRowIndex, column: column };
+
+  console.debug('clicked row: ', p);
+  requestAnimationFrame(() => draw());
+});
+
+canvas.addEventListener('contextmenu', (ev) => {
+  console.debug('ctx menu')
+  ev.preventDefault()
+})
+
+document.body.addEventListener('keydown', (ev) => {
+  if ((ev.target as any).tagName === 'TEXTAREA') {
+    const textarea = ev.target as HTMLTextAreaElement;
+
+    if (
+      textarea.selectionStart === 0 &&
+      textarea.selectionEnd === 0 && 
+      (ev.key === 'ArrowUp' || ev.key === 'ArrowLeft')
+    ) {
+      ev.preventDefault();
+    }
+    else if (
+      textarea.selectionStart === textarea.value.length &&
+      textarea.selectionEnd === textarea.value.length && 
+      (ev.key === 'ArrowDown' || ev.key === 'ArrowRight')
+    ) {
+      ev.preventDefault();
+    }
+    else
+    {
+      // other keypress in text area, abort and let the browser continue editing
+      return;
+    }
+  } else if (ev.key.indexOf('Arrow') === 0) {
+    ev.preventDefault();
+  }
+
+  if (editingColumn) {
+    switch (ev.key) {
+      case 'ArrowUp':
+      {
+        if (editingColumn.row > 0) {
+          editingColumn = {
+            row: editingColumn.row - 1,
+            column: editingColumn.column,
+          };
+        }
+        break;
+      }
+      case 'ArrowDown':
+      {
+        if (editingColumn.row + 1 < rows.length) {
+          editingColumn = {
+            row: editingColumn.row + 1,
+            column: editingColumn.column,
+          };
+        }
+        break;
+      }
+      case 'ArrowLeft':
+      {
+        if (editingColumn.column > 0) {
+          editingColumn = {
+            row: editingColumn.row,
+            column: editingColumn.column - 1,
+          };
+        }
+        break;
+      }
+      case 'ArrowRight':
+      {
+        if (editingColumn.column + 1 < columnHeaders.length) {
+          editingColumn = {
+            row: editingColumn.row,
+            column: editingColumn.column + 1,
+          };
+        }
+        break;
+      }
+    }
+  }
+
+  // TODO: If below or above current view port -> scroll
+
+  requestAnimationFrame(() => draw());
+}, true);
 
 requestAnimationFrame(() => draw());
